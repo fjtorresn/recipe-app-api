01e3fa4b51bc170d291cea48540d6d1e622507b0
fjtorresn <fco.torres12@gmail.com>
2025-06-28 21:10:06 -0400

    Add test examples

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return the result"""
+    return x + y
+
+
+def substract(x, y):
+    """Substract x and y and return the result"""
+    return y - x
