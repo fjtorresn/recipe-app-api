ddde075e51fc570769571c1cee14bac9f45433a4
fjtorresn <fco.torres12@gmail.com>
2025-07-02 22:28:34 -0400

    Customized admin site

--- app/core/models.py
+++ app/core/models.py
@@ -8,16 +8,30 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **kwargs):
         """Create, save and return a new user."""
-        user = self.model(email=email, **kwargs)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
